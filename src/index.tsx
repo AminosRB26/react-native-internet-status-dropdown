46bf99f66ced2659fa3fbd8de363bdebc0a061c5
Patrick <31239757+AminosRB26@users.noreply.github.com>
2024-07-10 13:08:20 +0100

    Update
    Added ts interface
    Improved main component

--- src/index.tsx
+++ src/index.tsx
@@ -1,44 +1,59 @@
-import React, { useEffect, useRef } from "react";
-import { Text, Animated, useWindowDimensions } from "react-native";
+import React, { ReactNode, useCallback, useEffect, useMemo, useRef } from "react";
+import { Text, Animated, useWindowDimensions, StyleProp, ViewStyle } from "react-native";
 import NetInfo from "@react-native-community/netinfo";
 import { styles } from "./styles";
-const InternetStatusBox = ({
+
+interface InternetStatusBoxProps {
+  duration: number;
+  backgroundColor?: string;
+  style?: StyleProp<ViewStyle>;
+  renderLabel?: ReactNode;
+  label?: string;
+  labelColor?: string;
+}
+
+const InternetStatusBox: React.FC<InternetStatusBoxProps> = ({
   backgroundColor = '#FFF',
-  slideDuration = 500,
-  containerStyle = {},
-  renderLabel = undefined,
-  label = '',
-  labelColor = '#111'
+  duration = 500,
+  style = {},
+  renderLabel,
+  label = 'No internet connection',
+  labelColor = '#111111',
 }) => {
 
   const { height, width } = useWindowDimensions();
 
   const progress = useRef(new Animated.Value(0)).current;
 
-  // Slides dropdown alert onto or off screen
-  const animate = (value: number) => {
-    Animated.timing(progress, {
-      toValue: value,
-      duration: slideDuration,
-      useNativeDriver: false,
-    }).start();
-  };
-
-  useEffect(() => {
-    NetInfo.addEventListener((state) => {
-      animate(state.isConnected ? 0 : 1);
-    });
-  }, []);
-
-  return (
-    <Animated.View style={[styles.container, {
+  const containerStyle = useMemo(() => {
+    return {
       backgroundColor,
       height: height * 0.065,
       width: width - 20,
       transform: [{
         translateY: progress.interpolate({ inputRange: [0, 1], outputRange: [-70, 30], }),
       }],
-    }, containerStyle]}>
+    }
+  }, [backgroundColor, height, width, progress])
+
+  const animate = useCallback((value) => {
+    Animated.timing(progress, {
+      toValue: value,
+      duration: duration,
+      useNativeDriver: false,
+    }).start();
+  }, [duration, progress]);
+
+  useEffect(() => {
+    const listener = NetInfo.addEventListener((state) => {
+      animate(state.isConnected ? 0 : 1);
+    });
+
+    return () => listener();
+  }, []);
+
+  return (
+    <Animated.View style={[styles.container, containerStyle, style]}>
       {renderLabel ?
         renderLabel :
         <Text style={[styles.label, { color: labelColor }]}>
